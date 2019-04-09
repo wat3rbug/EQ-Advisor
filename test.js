16a3fb6621765ee102c4b6c98a039d91bcc0af0a
Douglas Gardiner <doug.gardiner@gmail.com>
2019-04-09 14:56:20 -0500

    made first js script in a long time

--- test.js
+++ test.js
@@ -1,8 +1,16 @@
+
 function showPotGuide() {
-	$('#potModal').show();
+
+	$('#potModal').modal('show');
 }
 
-$(document).ready(){
-	var total = $('#pots').value() * 8;
+function calcPotions() {
+	var totalNeeded = $('#pots').val() * 4;
+	var blocks = $('#blocks').val() * 8;
+	var lgBricks = $('#lgBricks').val() * 4;
+	var smBricks = $('#smBricks').val() * 2;
+	var pieces = $('#smPieces').val();
+	var total = blocks + lgBricks + smBricks + pieces;
+	if (totalNeeded == totalNeeded) $('#potResult').text(total / 4);
 	
-};
+}
