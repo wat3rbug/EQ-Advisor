038038b3c01e0a6f9cfd1b646467f7aa45f5a10e
Douglas Gardiner <doug.gardiner@gmail.com>
2019-05-20 17:36:27 -0500

    try the move script

--- test.js
+++ test.js
@@ -1,17 +1,17 @@
-
-function addRecord() {
-
-	$('#addMeasurement').modal('show');
-	alert('it is called');
-}
-
-function a() {
-	var totalNeeded = $('#pots').val() * 4;
-	var blocks = $('#blocks').val() * 8;
-	var lgBricks = $('#lgBricks').val() * 4;
-	var smBricks = $('#smBricks').val() * 2;
-	var pieces = $('#smPieces').val();
-	var total = blocks + lgBricks + smBricks + pieces;
-	if (totalNeeded == totalNeeded) $('#potResult').text(total / 4);
+$(document).ready(
+	function showPotGuide() {
+		$('#potModal').modal('show');
+	}
 	
-}
+);
+
+// function showPotGuide() {
+// 	var totalNeeded = $('#pots').val() * 4;
+// 	var blocks = $('#blocks').val() * 8;
+// 	var lgBricks = $('#lgBricks').val() * 4;
+// 	var smBricks = $('#smBricks').val() * 2;
+// 	var pieces = $('#smPieces').val();
+// 	var total = blocks + lgBricks + smBricks + pieces;
+// 	if (totalNeeded == totalNeeded) $('#potResult').text(total / 4);
+//
+// }
