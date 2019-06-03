2ab234e35fbb144a3a2b0fa4e739bd5408f0953a
Douglas Gardiner <doug.gardiner@gmail.com>
2019-06-03 16:09:09 -0500

    working on website for portfolio

--- test.js
+++ test.js
@@ -6,22 +6,23 @@ $(document).ready(function() {
 });
 
 function calcPotions() {
+	
+	// there has to be a better way to ensure proper casting of values
+	
 	var totalNeeded = $('#pots').val() * 4;
 	var blocks = $('#blocks').val() * 8;
 	var lgBricks = $('#lgBricks').val() * 4;
 	var smBricks = $('#smBricks').val() * 2;
-	var pieces = $('#smPieces').val();
-	var total = blocks + lgBricks + smBricks + pieces;
-	if (total == total) $('#potResult').text(total / 4);
+	var pieces = $('#smPieces').val() * 1;
+	var totalAcquired = blocks + lgBricks + smBricks + pieces;
+	var total = Math.floor(totalAcquired / 4);
+	if (total == total) {	// because NaN is NOT = to itself, basic check
+		$('#potResult').text(total);
+		if (totalAcquired >= totalNeeded) {
+			$("#potResult").addClass("text-success").removeClass("text-danger");
+		} else {
+			$("#potResult").removeClass("text-success").addClass("text-danger");
+		}
+	}	
 };
 
-// function showPotGuide() {
-// 	var totalNeeded = $('#pots').val() * 4;
-// 	var blocks = $('#blocks').val() * 8;
-// 	var lgBricks = $('#lgBricks').val() * 4;
-// 	var smBricks = $('#smBricks').val() * 2;
-// 	var pieces = $('#smPieces').val();
-// 	var total = blocks + lgBricks + smBricks + pieces;
-// 	if (totalNeeded == totalNeeded) $('#potResult').text(total / 4);
-//
-// }
